c4833f4a6bfb4eb4285c7e0f2d4caaa933ceac62
kanghyeon2 <topskh@naver.com>
2025-02-16 17:19:19 +0900

    리스트 출력 페이징X HTML파일 디비 임의 저장

---
+++ server/src/main/java/egovframework/com/approval/service/DocumentService.java
@@ -0,0 +1,9 @@
+package egovframework.com.approval.service;
+
+import java.util.List;
+
+public interface DocumentService {
+	public int documentInsert(DocumentDTO documentDTO);
+	
+	public List<DocumentDTO> getList(SearchDTO searchDTO);
+}
