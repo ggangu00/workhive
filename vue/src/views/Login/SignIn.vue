6c317f22071f0e9d8aa89367686078f98f02a718
mjin0302 <alswls7887@gmail.com>
2025-02-19 02:49:10 +0900

    로그인 화면 수정중

--- vue/src/views/Login/SignIn.vue
+++ vue/src/views/Login/SignIn.vue
@@ -7,30 +7,33 @@
       <div class="">
          <div class="login_box">
             <div class="input_item id on" id="input_item_id">
-               <input type="text" id="id" name="id" accesskey="L" maxlength="41" autocapitalize="none" value="" title="아이디" class="input_id" aria-label="아이디 또는 전화번호">
-               <label for="id" class="text_label" id="id_label" aria-hidden="true">아이디 또는 전화번호</label>
+               <input type="text"  title="아이디" class="input_id" aria-label="아이디 또는 전화번호">
+               <label class="text_label" id="id_label" aria-hidden="true">아이디 또는 전화번호</label>
                <button type="button" class="btn_delete" id="id_clear" style="display: block;">
-                  <span class="icon_delete"><span class="blind">삭제</span></span>
+                  <i class="fa-solid fa-xmark"></i>
                </button>
             </div>
             <div class="input_item pw on" id="input_item_pw">
                <input type="password" id="pw" name="pw" title="비밀번호" class="input_pw" maxlength="16" aria-label="비밀번호">
                <label for="pw" class="text_label" id="pw_label" aria-hidden="true">비밀번호</label>
                <button type="button" class="btn_view hide" id="pw_hide" aria-pressed="false" style="display: block;">
-                  <span class="icon_view"><span class="blind" id="icon_view">비밀번호 표시</span></span>
+                  <i class="fa-solid fa-lock"></i><i class="fa-solid fa-lock-open"></i>
                </button>
                <button type="button" class="btn_delete" id="pw_clear" style="display: block;">
-                  <span class="icon_delete"><span class="blind">삭제</span></span>
+                  <i class="fa-solid fa-xmark"></i>
                </button>
             </div>
+            <!-- 로그인 상태 유지 -->
+            <div class="login_keep_wrap" id="login_keep_wrap">
+               <div class="keep_check">
+                  <input type="checkbox" id="keep" name="nvlong" class="input_keep" value="off">
+                  <label for="keep" class="keep_text">로그인 상태 유지</label>
+               </div>
+            </div>
+
          </div>
 
-         <div class="login_keep_wrap" id="login_keep_wrap">
-            <div class="keep_check">
-               <input type="checkbox" id="keep" name="nvlong" class="input_keep" value="off">
-               <label for="keep" class="keep_text">로그인 상태 유지</label>
-            </div>
-         </div>
+
          <div class="btn_login_wrap">
             <button type="submit" class="btn_login next_step nlog-click" id="log.login">로그인</button>
          </div>
@@ -92,7 +95,7 @@ img {
 h1 {
    font-size: 32px;
    font-weight: bold;
-   color: #555;
+   color: #222222;
 }
 
 /* 로그인 박스 */
