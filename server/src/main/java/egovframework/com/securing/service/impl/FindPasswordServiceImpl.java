1715b7eee1fc8352b5e655cd252d694d72763322
mjin0302 <alswls7887@gmail.com>
2025-03-03 02:06:27 +0900

    서비스 어노테이션 추가

--- server/src/main/java/egovframework/com/securing/service/impl/FindPasswordServiceImpl.java
+++ server/src/main/java/egovframework/com/securing/service/impl/FindPasswordServiceImpl.java
@@ -1,7 +1,10 @@
 package egovframework.com.securing.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import egovframework.com.securing.service.FindPasswordService;
 
+@Service("findPasswordService")
 public class FindPasswordServiceImpl implements FindPasswordService {
 
 }
