7f1cfd2eeaf8826d8755fc5a313a2da9b6864067
jin ◡̈ <alswls7887@gmail.com>
2025-03-07 14:28:26 +0900

    부서 트리형태

--- vue/src/components/Department/DepartmentComponent.vue
+++ vue/src/components/Department/DepartmentComponent.vue
@@ -1,11 +1,14 @@
 <template>
-   <Tree :value="props.departmentTree" class="department-tree" />
+   <Tree :value="props.departmentTree" class="custom-tree">
+      <template #togglericon="{ expanded }">
+         <i :class="expanded ? 'fa-solid fa-angle-down' : 'fa-solid fa-angle-right'"></i>
+      </template>
+   </Tree>
 </template>
 
-
 <script setup>
-import { watch, onMounted, defineProps } from 'vue';
 import Tree from 'primevue/tree'
+import { defineProps } from 'vue'
 
 const props = defineProps({
    departmentTree: {
@@ -13,16 +16,50 @@ const props = defineProps({
       default: () => []
    }
 })
-
-onMounted(() => {
-   console.log("부모가 넘겨준 departmentTree:", props.departmentTree)
-})
-
-watch(() => props.departmentTree, (newVal) => {
-   console.log("departmentTree 변경 감지:", newVal)
-})
 </script>
 
-<style scoped>
+<style lang="scss">
 
+   /* 트리 전체 */
+   .custom-tree {
+      padding: 0;
+      border: none;
+   }
+
+   .custom-tree .p-tree-container {
+      padding-left: 0px;
+   }
+
+   /* 각 노드 */
+   .custom-tree .p-treenode {
+      margin: 4px 0;
+      padding: 0;
+   }
+
+   /* 노드 내용 (아이콘 + 텍스트) */
+   .custom-tree .p-treenode-content {
+      display: flex;
+      align-items: center;
+      height: 30px;
+      border-radius: 3px;
+      transition: background-color 0.2s;
+   }
+
+   /* 펼침/닫힘 아이콘 */
+   .custom-tree .p-tree-toggler {
+      font-size: 12px;
+      color: #747474;
+   }
+
+   /* 노드 텍스트 */
+   .custom-tree .p-treenode-label {
+      font-size: 14px;
+      color: #747474;
+      font-weight: 600;
+   }
+
+   /* 자식 없는 노드의 토글버튼 강제 숨김 */
+   .custom-tree .p-treenode-leaf .p-tree-toggler {
+      display: none !important;
+   }
 </style>
